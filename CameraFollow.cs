dd56d9e82fb094b14b8963cffdd91d1783516434
saurabhcool001 <parmarsaurabh87@gmail.com>
2021-07-12 23:24:03 +0530

    Update CameraFollow.cs

--- CameraFollow.cs
+++ CameraFollow.cs
@@ -9,7 +9,6 @@ public class CameraFollow : MonoBehaviour
     public float smoothSpeed = 0.125f;
     public float smoothTime = 0.3f;
     public Vector3 offset;
-    //Vector3 velocity = Vector3.zero;
     public Vector3 focusedPos;
     public Vector3 crushedPos;
     public Vector3 finishPos;
@@ -17,8 +16,6 @@ public class CameraFollow : MonoBehaviour
     public bool isTriggerCrushCam;
 
     public Vector3 currOffset;
-
-    //private float jumpIter = 9.5f;
     public float shakeAmt =  4f; // the degrees to shake the camera
     public float shakePeriodTime = 0.25f; // The period of each shake
     public float dropOffTime = 0.5f; // How long it takes the shaking to settle down to nothing
@@ -39,7 +36,6 @@ public class CameraFollow : MonoBehaviour
         {
             Vector3 desiredPosition = target.position + currOffset;
             Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-            //Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothTime, smoothSpeed);
             transform.position = smoothedPosition;
             transform.LookAt(target);
         }
@@ -69,8 +65,6 @@ public class CameraFollow : MonoBehaviour
     [EasyButtons.Button]
     internal void CamShake()
     {
-        //float height = Mathf.PerlinNoise(jumpIter, 0f) * 10f;
-
         LTDescr shakeTween = LeanTween.rotateAroundLocal(transform.parent.gameObject, Vector3.one, shakeAmt, shakePeriodTime/2)
         .setEase(LeanTweenType.easeShake) // this is a special ease that is good for shaking
         .setLoopClamp()
