2e6c8410a50a86266f0307c328f726b6f974e613
saurabhcool001 <parmarsaurabh87@gmail.com>
2021-07-12 23:23:16 +0530

    Update Ball.cs

--- Ball.cs
+++ Ball.cs
@@ -18,22 +18,20 @@ public class Ball : MonoBehaviour
 
     void Update()
     {
-        //refVelocity = ballBody.velocity;
+  
         if (Input.GetMouseButton(0) && Player.Instance.playerState == State.Throw)
         {
             if (Input.GetAxis("Mouse X") != 0)
             {
                 sideDragSensitivity = ballBody.velocity.z * 2 *  Time.deltaTime; 
                 ballRot.x = (Input.GetAxis("Mouse X") - Input.GetAxis("Mouse Y")) * sideDragSensitivity;
-                //ballBody.position += Vector3.SmoothDamp(Vector3.zero, ballRot, ref refVelocity, 3, sideDragSensitivity);
-                //if (ballBody.velocity.z > minVelocityThreshold)
-                //{
+
 
                     ballBody.velocity += ballRot;
-                //}
+
             }
         }
-        //print("vel " + ballBody.velocity.z);
+       
     }
 
 
@@ -53,11 +51,6 @@ public class Ball : MonoBehaviour
             Player.Instance.enemyBloodFX.transform.position = other.transform.position;
             Player.Instance.enemyBloodFX.Play();
             other.GetComponent<Animator>().SetBool("isDead_" + Random.Range(1, 4), true);
-            //other.isTrigger = false;
-            //other.attachedRigidbody.isKinematic = false;
-            //forceDir = transform.position - other.transform.position;
-            //other.attachedRigidbody.AddForce(Vector3.forward * Random.Range(20, 40), ForceMode.VelocityChange);
-            print("zFOund");
             EnemyManager.Instance.Dead(other.gameObject);
 
             ballBody.AddRelativeForce(transform.forward * 5, ForceMode.VelocityChange);
